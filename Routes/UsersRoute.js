0a982e92e8591a22a2a75ec8d6c127c54d7da49e
Rohit Rajput <rajputrohitbcs@gmail.com>
2024-11-13 17:57:10 +0530

    remove auth

--- Routes/UsersRoute.js
+++ Routes/UsersRoute.js
@@ -1,6 +1,6 @@
 import express from"express";
 import { login, logout, register, updateProfile } from "../Controllers/usercontroller.js";
-import isAuthenticate from "../Middleware/isAuthenticate.js";
+//import isAuthenticate from "../Middleware/isAuthenticate.js";
 import { singleUpload } from "../Middleware/multer.js";
 
 
@@ -10,6 +10,6 @@ const router =express.Router();
 router.route('/register').post(singleUpload,register)
 router.route('/login').post(login)
 router.route("/logout").get(logout)
-router.route('/profile/update').post(isAuthenticate,singleUpload,updateProfile)
+router.route('/profile/update').post(singleUpload,updateProfile)
 
 export default router;
