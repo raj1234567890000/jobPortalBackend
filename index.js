afc90d99ef59bee1d8b39e0e166c7c7d1aa03283
Rohit Rajput <rajputrohitbcs@gmail.com>
2024-11-13 12:54:29 +0530

    originc change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(morgan('dev'))
 const corsOptions={
-    origin:"https://jobportal-29rz.onrender.com",
+    origin:"https://careernest-j9hf.onrender.com",
     credentials:true,
 }
 app.use(cors(corsOptions));
