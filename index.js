b583c756d37df92716dba24c3b430065b6d8ffd7
Rohit Rajput <rajputrohitbcs@gmail.com>
2024-12-20 17:07:51 +0530

    hhfhf

--- index.js
+++ index.js
@@ -4,47 +4,60 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from 'dotenv';
 import connectDB from "./utils/db.js";
-import userRoute from './Routes/UsersRoute.js';
-import CompanyRoute from './Routes/companyroute.js';
-import JobRoute from "./Routes/jobroute.js";
-import ApplicationRoute from "./Routes/applicationroute.js";
+import userRoute from './Routes/UsersRoute.js'
+import CompanyRoute from './Routes/companyroute.js'
+import JobRoute from "./Routes/jobroute.js"
+import ApplicationRoute from "./Routes/applicationroute.js"
 import path from 'path';
-import { URL } from "url";
 
-const app = express();
+
+
+
+
+const app=express();
 dotenv.config({});
-const __dirname = new URL('.', import.meta.url).pathname;
 
-// Middleware
+
+
+//middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(morgan('dev'));
-
+app.use(morgan('dev'))
 const corsOptions = {
     origin: process.env.CLIENT_URL || "https://jobportalclient-3hoh.onrender.com",
     credentials: true,
 };
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+
+//rest api
 
 // View engine setup
 app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.resolve("views"));
 app.use(express.static("public"));
 
 // Routes
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-});
+app.get('/',(req,res)=>{
+    res.send('<h1>hello backend server</h1>')
+})
 
-// API routes
-app.use('/api/v1/user', userRoute);
-app.use('/api/v1/company', CompanyRoute);
-app.use('/api/v1/job', JobRoute);
-app.use('/api/v1/application', ApplicationRoute);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+
+//all apis here
+app.use('/api/v1/user',userRoute)
+app.use('/api/v1/company',CompanyRoute )
+app.use('/a1/v1/job',JobRoute)
+app.use('/a1/v1/application',ApplicationRoute)
+
+
+
+
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>{
     connectDB();
-    console.log(`Server running at port ${PORT}`);
-});
+    console.log(`server running at port ${PORT}`)
+
+})
